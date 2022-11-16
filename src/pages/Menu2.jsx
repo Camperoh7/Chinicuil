60e4841c93047964c102ac490bb2621f3c6202a9
Camperoh7 <gibran_h7@hotmail.com>
2022-11-16 11:08:51 -0500

    Menú actualizado 16/11/22

--- src/pages/Menu2.jsx
+++ src/pages/Menu2.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+
 
 const Menu2 = () => {
 return (
@@ -8,20 +8,25 @@ return (
         <div className='menu'>
         <div className='encabezado parte_izquierda'>
             <h3>Desayunos</h3>
-            <p>- Desayuno Completo <strong>$180</strong>. (Incluye yogurth con cereal, jugo de temporada, café de olla y huevos al gusto ó chilaquiles ó flautas de pollo/papa)</p>
+            <p>- Desayuno Completo. (Incluye yogurth con cereal, jugo de temporada, café de olla y huevos al gusto ó chilaquiles ó flautas de pollo/papa) <strong>$180</strong></p>
             <p>- Omelette de Espinaca con Queso de Cabra <strong>$150</strong></p>
             <p>- Omelette de Hongos con Quesillo <strong>$150</strong></p>
             <p>- Omelette de Huitlacoche <strong>$150</strong></p>
             <p>- Omelette de Jamón con Quesillo <strong>$150</strong></p>
             <p>- Chilaquiles con Pollo o Huevo (Verdes o Rojos) <strong>$150</strong>. Con extra de huevo <strong>+$25</strong>, con extra de cecina <strong>+$60</strong>, con extra de pollo <strong>+$50</strong>, con extra de chorizo <strong>+$30</strong></p>
             <p>- Fruta de Temporada <strong>$75</strong></p>
+            <p>- Pan Tostado con Mantequilla y Mermelada <strong>$45</strong></p>
             <p>- Huevos Rancheros <strong>$150</strong></p>
             <p>- Huevos con Mole Poblano <strong>$150</strong></p>
             <p>- Huevos a la Mexicana <strong>$150</strong></p>
-            <img className='xolo' src='images/xoloitzcuintle.jpg'></img>
+            <img className='xolo' src='images/xoloitzcuintle.jpg'/>
             <h3>Entradas</h3>
             <p>- Guacamole <strong>$220</strong></p>
-            <p>- Nachos con Carne <strong>$280</strong></p><hr/>
+            <p>- Nachos con Carne <strong>$280</strong></p>
+            <p>- Tlacoyos (Orden de 3pz) <strong>$60</strong>. Con pollo <strong>+$50</strong>, con cecina <strong>+$60</strong>, con arrachera <strong>+$80</strong></p>
+            <p>- Sopes (Orden de 3pz) <strong>$90</strong>. Con pollo <strong>+$50</strong>, con cecina <strong>+$60</strong>, con arrachera <strong>+$80</strong></p>
+            <p>- Quesadilla de Queso <strong>$110</strong></p>
+            <p>- Sope con Tuetano <strong>$140</strong></p><hr/>
             <h3>Platillos</h3>
             <p>- Molcajete Mar y Tierra. (Frijoles rancheros, arrachera, camarones, queso hecho por chinicuil, chorizo, nopales, cebollines, chile serrano y tortillas hechas a mano 6pz) Extra de tortillas 6pz <strong>+$25.  </strong> <strong>$720</strong></p>
             <p>- Pollo Entero Estilo Hidalgo. (En penca de Maguey con adobo y nopales) <strong>$450</strong></p>
@@ -32,14 +37,14 @@ return (
             <p>- Camarones al Axiote <strong>$350</strong></p>
             <p>- Camarones al Ajillo <strong>$350</strong></p>
             <p>- Arrachera Chinicuil <strong>$350</strong></p>
-            <p>- Tlacoyos (Orden de 3pz) <strong>$60</strong>. Con pollo <strong>+$50</strong>, con cecina <strong>+$60</strong>, con arrachera <strong>+$80</strong></p>
-            <p>- Sopes (Orden de 3pz) <strong>$90</strong>. Con pollo <strong>+$50</strong>, con cecina <strong>+$60</strong>, con arrachera <strong>+$80</strong></p>
             <p>- Taco de Cecina <strong>$45</strong></p>
             <p>- Taco de Arrachera <strong>$65</strong></p>
+            <p>- Quesadilla de Pollo <strong>$150</strong></p>
+            <p>- Queso Fresco Asado (Acompañado de tortillas hechas a mano y a elegir: champiñones, huitlacoche ó espinaca) <strong>$160</strong></p>
             <p>- Caldo de Pollo <strong>$100</strong></p>
             <p>- Sopa de Tortilla <strong>$130</strong></p>
             <p>- Sopa del Día <strong>$130</strong></p>
-            <img className='xolo' src='images/agave.jpg'></img>
+            <img className='xolo' src='images/agave.jpg'/>
             <h3>Vegano</h3>
             <p>- Quesadilla de Hongos <strong>$110</strong></p>
             <p>- Quesadilla de Huitlacoche <strong>$130</strong></p>
@@ -48,15 +53,22 @@ return (
             <p>- Enchiladas Rellenas de Hongos (Verdes o Rojas) <strong>$180</strong></p><hr/>
             <h3>Todos los Jueves y los Viernes</h3>
             <p>- Pozole <strong>$180</strong></p>
-            <img src='images/gusano-hoja.WEBP'></img><hr/>
+            <img src='images/gusano-hoja.WEBP'/><hr/>
             <h3>Todos los Sábados y Domingos</h3>
-            <p>- Tacos de Barbacoa <strong>$45</strong></p>
-            <p>- Tacos de Pancita <strong>$55</strong></p>
+            <p>- Taco de Barbacoa <strong>$45</strong></p>
+            <p>- Taco de Barbacoa en Salsa Verde con Nopales <strong>$55</strong></p>
+            <p>- Chilaquiles con Barbacoa <strong>$180</strong></p>
+            <p>- Taco de Pancita <strong>$55</strong></p>
             <p>- Flautas de Barbacoa (Orden de 3pz) <strong>$150</strong></p>
-            <p>- Quesadillas de Barbacoa <strong>$140</strong></p>
+            <p>- Quesadilla de Barbacoa <strong>$150</strong></p>
             <p>- Consome con carne <strong>$90</strong></p>
-            <p>- Consome sin carne <strong>$50</strong></p>
-            <img className='xolo bw' src='images/barbacoa2.jpg'></img>
+            <p>- Consome sin carne <strong>$55</strong></p>
+            <img className='xolo bw' src='images/barbacoa2.jpg'/>
+            <h3>Postres</h3>
+            <p>- Gelatina de Mosaico <strong>$75</strong></p>
+            <p>- Arroz con Leche <strong>$75</strong></p>
+            <p>- Plátano Frito <strong>$75</strong></p>
+            <img className='xolo bw' src='images/postre1.jpg'/>
             <h3>Bebidas</h3>
             <p>- Café de Olla <strong>$45</strong></p>
             <p>- Café Americano <strong>$60</strong></p>
@@ -70,19 +82,22 @@ return (
             <p>- Montejo <strong>$50</strong></p>
             <p>- Negra Modelo <strong>$60</strong></p>
             <p>- Modelo Especial <strong>$60</strong></p>
-            <img className='xolo bw' src='images/cantarito.WEBP'></img>
+            <p>- Cerveza Victoria <strong>$50</strong></p>
+            <img className='xolo bw' src='images/cantarito.WEBP'/>
             <h3>Cocteles</h3>
             <p>- MARGARITA <strong>$150</strong></p>
             <p>- MEZCALITA <strong>$220</strong></p>
             <p>- MOJITO <strong>$150</strong></p>
-            <p>- CANTARITO <strong>$150</strong></p><hr/>
+            <p>- CANTARITO <strong>$150</strong></p>
+            <p>- TEQUILA SUNRISE <strong>$150</strong></p>
+            <p>- PIÑA COLADA <strong>$150</strong></p><hr/>
             <h3>Tequila</h3>
             <p>- Don Julio reposado <strong>$200</strong></p>
             <p>- Mestro Dobel <strong>$220</strong></p>
             <p>- Herradura Ultra <strong>$250</strong></p>
             <p>- Patrón Silver <strong>$260</strong></p>
             <p>- José Cuervo Especial <strong>$180</strong></p>
-            <img className='xolo bw' src='images/chinicuil-foto.jpg'></img>
+            <img className='xolo bw' src='images/chinicuil-foto.jpg'/>
             <h3>Mezcal</h3>
             <p>- Etereo Espadín<strong>$180</strong></p>
             <p>- 400 Conejos joven<strong>$180</strong></p>
@@ -91,21 +106,20 @@ return (
             <h3>Ginebra</h3>
             <p>- Tanqueray Ten <strong>$250</strong></p>
             <p>- Bombay <strong>$250</strong></p>
-            <img src='images/gusano-feliz.jpg'></img>
+            <img src='images/gusano-feliz.jpg'/>
             <h3>Whisky</h3>
             <p>- Buchanan´s 12 <strong>$200</strong></p>
             <p>- Macallan 12 <strong>$300</strong></p><hr/>
             <h3>Meet Malix</h3>
-            <img className='xolox' src='images/malix1.jpeg'></img>
-            <img className='xolox' src='images/malix2.jpeg'></img>
-            <img className='xolox' src='images/malix3.jpeg'></img>
-            <img className='xolox' src='images/malix4.jpeg'></img><hr/>
-            <img className='insta' src='images/instagram.png'>
-            </img>
+            <img className='xolox' src='images/malix1.jpeg'/>
+            <img className='xolox' src='images/malix2.jpeg'/>
+            <img className='xolox' src='images/malix3.jpeg'/>
+            <img className='xolox' src='images/malix4.jpeg'/><hr/>
+            <img className='insta' src='images/instagram.png'/>
             <a href={'https://www.instagram.com/chinicuil.holbox/'} className='chin'>
             @chinicuil.holbox *click here!*
             </a><br/>
-            <img className='insta' src='images/star-png.webp'></img>
+            <img className='insta' src='images/star-png.webp'/>
             <a href={'https://www.google.com/search?q=chinicuil+holbox&oq=chinicuil+holbox&aqs=chrome.0.0i355i512j46i175i199i512j69i60l2.3217j0j4&sourceid=chrome&ie=UTF-8#lrd=0x8f4d93b6b28fc8a3:0x28a8d8f6cf1f195b,3,,,'} className='chin'>
             ¡Danos Tu Opinión! *click here!*
             </a>
